724b3c70151a1d91612762d3d0536ba43a5b27ff
Jimmy <joony300@gmail.com>
2023-07-25 17:48:52 +0900

    [Add] UserDefault 생성

---
+++ EmotionApp/EmotionManager.swift
@@ -0,0 +1,21 @@
+//
+//  EmotionManager.swift
+//  EmotionApp
+//
+//  Created by 정준영 on 2023/07/25.
+//
+
+import Foundation
+
+struct EmotionManager {
+
+    enum Emotion: Int, CaseIterable {
+        case veryHappy = 0
+        case moderateSmile
+        case neutral
+        case slightlyUpset
+        case verySad
+    }
+    
+    
+}
---
+++ EmotionApp/UserDefault.swift
@@ -0,0 +1,60 @@
+//
+//  UserDefault.swift
+//  EmotionApp
+//
+//  Created by 정준영 on 2023/07/25.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    private let key: String
+    private let defaultValue: T
+    
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+struct VeryHappyIndex {
+    @UserDefault(key: keyEnum.verySadIndex.rawValue, defaultValue: 0)
+    static var count: Int
+}
+struct ModerateSmileIndex {
+    @UserDefault(key: keyEnum.moderateSmileIndex.rawValue, defaultValue: 0)
+    static var count: Int
+}
+struct NeutralIndex {
+    @UserDefault(key: keyEnum.neutralIndex.rawValue, defaultValue: 0)
+    static var count: Int
+}
+struct SlightlyUpsetIndex {
+    @UserDefault(key: keyEnum.slightlyUpsetIndex.rawValue, defaultValue: 0)
+    static var count: Int
+}
+struct verySadIndex {
+    @UserDefault(key: keyEnum.verySadIndex.rawValue, defaultValue: 0)
+    static var count: Int
+}
+
+enum keyEnum: String {
+    case veryHappyIndex
+    case moderateSmileIndex
+    case neutralIndex
+    case slightlyUpsetIndex
+    case verySadIndex
+}
+
+
+
